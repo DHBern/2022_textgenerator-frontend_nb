077345706cbfd230b1c1f462b369617fa4910550
Sebastian <sebastian@flicks.jetzt>
2022-06-16 18:55:26 +0200

    implement api-calls

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,11 +1,29 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
+
+/**
+ * @param defaults {Object}
+ */
+function createConfig(defaults) {
+  const { subscribe, set } = writable(defaults);
+
+  return {
+    subscribe,
+    set,
+    reset: () => set({
+        /** @type {""|"hennings"|"walser"} */
+        author: '',
+        input: '',
+        temp: 0.6
+    }),
+  }
+}
 
 /**
  * @param value {number}
  */
-export const config = writable({
-    /** @type {""|"hennings"|"walser"} */
-    author: "",
-    input: "",
-    temp: 0.6,
-})
+export const config = createConfig({
+	/** @type {""|"hennings"|"walser"} */
+	author: '',
+	input: '',
+	temp: 0.6
+});
--- src/routes/result.svelte
+++ src/routes/result.svelte
@@ -1,27 +1,78 @@
 <script>
-    import { config } from "$lib/stores";
-    import Print from "$lib/Print.svelte";
-    import Qrcode from "$lib/Qrcode.svelte";
-    import { goto } from "$app/navigation";
-    import { onMount } from 'svelte';
+	import { config } from '$lib/stores';
+	import Print from '$lib/Print.svelte';
+	import Qrcode from '$lib/Qrcode.svelte';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 
-    onMount(async () => {
-        // Implement logic for either sending a request to the server or rerouting to the home page.
-        if ($config.author && $config.temp && $config.input) {
-            // Send request to server.
-            // implement the call to the huggingface API
-        } else {
-            console.log("no author or temp or input, redirecting to home");
-            goto("/");
-        }
-    });
+	let queryCounter = 0;
+    /** @type {[{generated_text: string}]} */
+	let sentences = [{generated_text: 'Lade Sätze...'}];
 
+	async function query(data) {
+		const response = await fetch(
+			`https://api-inference.huggingface.co/models/dh-unibe/gpt2-ger-${$config.author}`,
+			{
+				headers: { Authorization: `Bearer ${import.meta.env.VITE_HUGGINGFACE_KEY}` },
+				method: 'POST',
+				body: JSON.stringify(data)
+			}
+		);
+		if (response.ok) {
+			const result = await response.json();
+			console.log(result);
+			return result;
+		} else {
+			queryCounter++;
+			console.log('failed');
+			console.log(queryCounter);
+			data.wait_for_model = true;
+			return queryCounter <= 30 ? query(data) : [{ generated_text: 'Da ging etwas bei der Abfrage schief.' }];
+		}
+	}
+
+	const payload = {
+		inputs: `Ich denke über ${$config.input},`,
+		temperature: $config.temp,
+		top_k: 50,
+		max_length: 1000,
+		num_return_sequences: 20,
+		wait_for_model: false,
+		use_cache: false
+	};
+
+	onMount(async () => {
+		if ($config.author && $config.temp && $config.input) {
+			sentences = await query(payload);
+		} else {
+			console.log('no author or temp or input, redirecting to home');
+			goto('/');
+		}
+	});
 </script>
 
-<p>"This is the sentence from the server."</p>
+{#if sentences.error}
+	<p class="error">{sentences.error}</p>
+{/if}
+
+{#if sentences[0].generated_text === 'Lade Sätze...'}
+    // TODO: show loading indicator
+    <p>'Lade Sätze...'</p>
+
+{:else}
+
+    {#each sentences as sentence}
+        <p>{JSON.stringify(sentence.generated_text)}</p>
+    {/each}
+
+    <a href="/" on:click={config.reset}>Zurück zur Startseite</a>
+
+{/if}
+
 <Print />
 <Qrcode />
 
-<style lang="scss">
+{JSON.stringify($config)}
 
+<style lang="scss">
 </style>
