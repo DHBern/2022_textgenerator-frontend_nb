f8e726d77c47607ad66d6b42d0961134f89c14df
Sebastian <sebastian@flicks.jetzt>
2022-06-16 11:12:07 +0200

    redirect if config is not set

--- src/routes/result.svelte
+++ src/routes/result.svelte
@@ -2,14 +2,19 @@
     import { config } from "$lib/stores";
     import Print from "$lib/Print.svelte";
     import Qrcode from "$lib/Qrcode.svelte";
+    import { goto } from "$app/navigation";
+    import { onMount } from 'svelte';
 
-    // Implement logic for either sending a request to the server or rerouting to the home page.
-    if (config.author && config.temp && config.input) {
-        // Send request to server.
-        // implement the call to the huggingface API
-    } else {
-        // Reroute to home page.
-    }
+    onMount(async () => {
+        // Implement logic for either sending a request to the server or rerouting to the home page.
+        if ($config.author && $config.temp && $config.input) {
+            // Send request to server.
+            // implement the call to the huggingface API
+        } else {
+            console.log("no author or temp or input, redirecting to home");
+            goto("/");
+        }
+    });
 
 </script>
 
