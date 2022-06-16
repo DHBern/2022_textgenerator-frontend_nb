c90708030e71e26d3cbad78d2c7c8fda84c8cc68
Sebastian <sebastian@flicks.jetzt>
2022-06-16 10:36:28 +0200

    fallback and dev-output for index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,11 +2,14 @@
 import Author from "$lib/Author.svelte";
 import Config from "$lib/Config.svelte";
 import { config } from "$lib/stores";
-
 </script>
 
 {#if !$config.author}
     <Author />
 {:else if !$config.temp || !$config.input}
     <Config />
+{:else}
+    <a href="/result">to the result!</a>
 {/if}
+
+{ JSON.stringify($config) }
