684de6d611bd43d8f195f50c6078bb3b13afd30a
Sebastian <sebastian@flicks.jetzt>
2022-06-16 10:36:07 +0200

    typing for config

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,7 +1,11 @@
 import { writable } from "svelte/store";
 
+/**
+ * @param value {number}
+ */
 export const config = writable({
+    /** @type {""|"hennings"|"walser"} */
     author: "",
     input: "",
-    temp: 0,
+    temp: 0.6,
 })
