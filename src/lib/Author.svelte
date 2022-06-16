55a97ac625506ef038e7addb1ed981fd6e839672
Sebastian <sebastian@flicks.jetzt>
2022-06-16 10:38:28 +0200

    populate author and config modules

--- src/lib/Author.svelte
+++ src/lib/Author.svelte
@@ -1,11 +1,22 @@
 <script>
-
+import { config } from "$lib/stores";
 </script>
 
-<div class="author_container">
+<section class="author_container" on:click={() => $config.author='hennings'}>
     <img alt="Emmy Hennings" />
+    <h2>Emmy Hennings</h2>
+    <p>
+        Emmy Hennings is a web developer and designer.
+    </p>
+</section>
+
+<section class="author_container" on:click={() => $config.author='walser'}>
     <img alt="Robert Walser" />
-</div>
+    <h2>Robert Walser</h2>
+    <p>
+        Robert Walser is a web developer and designer.
+    </p>
+</section>
 
 <style>
 
---
+++ src/lib/Config.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { config } from "$lib/stores";
+</script>
+
+<section>
+<h2>Please select the temperature:</h2>
+<input type="range" min=0.3 max=0.9 step=0.3 bind:value={$config.temp} />
+</section>
+
+<a href="/result">to the result!</a>
