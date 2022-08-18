e92c5d3f058915f01fb09263c107290426a67c0b
Sebastian <sebastian@flicks.jetzt>
2022-08-18 20:09:56 +0200

    add animations on start-page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,38 +3,64 @@
 	import Config from '$lib/Config.svelte';
 	import { config } from '$lib/stores';
 	import info from '$lib/assets/text.json';
+	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+	import { OnMount } from 'fractils';
+
+	/**
+	 * @type {boolean}
+	 */
+	let ready;
+
+	const sentences = [
+		'Hallo!',
+		'kommen Sie näher...',
+		'Nur keine Angst.',
+		'Ich bin intelligent, künstlich intelligent.',
+		`Ich schreibe Literatur. Ich kann Robert Walser und Emmy Hennings.`
+	];
+
+	/**
+	 * @type {[string]|[]}
+	 */
+	let activeSentences = [];
+
+	onMount(() => {
+		for (let i = 0; i < sentences.length; i++) {
+			setTimeout(() => {
+				activeSentences.push(sentences[i]);
+				activeSentences = activeSentences;
+			}, i * 3500);
+		}
+	});
 </script>
 
 <div class="container">
-	{#if $config.author}
-		<!--<h1>
-		<span class="accent-writing"
-			>{info[$config.author].fullname}</span
-		> zum Schreiben bringen
-	</h1>-->
-	{/if}
 	<main>
 		{#if !$config.author}
 			<section class="intro">
-				<p>
-					Hallo, kommen Sie näher. Nur keine Angst. Ich bin intelligent, künstlich intelligent. Ich
-					schreibe Literatur. Ich kann
-					<button on:click={() => ($config.author = 'walser')}>Robert Walser</button> und
-					<button on:click={() => ($config.author = 'hennings')}>Emmy Hennings</button>.
-				</p>
+				{#each activeSentences as sentence}
+					<p transition:fade>{@html sentence}</p>
+				{/each}
 			</section>
-			<h1>Wer soll für Sie schreiben?</h1>
 		{/if}
-		<div class="config-container {$config.author ? 'config-container--form' : ''}">
-			{#if $config.author}
-				<Config />
-				<div class="authorbox">
+		<OnMount>
+			<h1 in:fade={{ delay: 15000 }} out:fade>Wer soll für Sie schreiben?</h1>
+			<div
+				in:fade={{ delay: 16000 }}
+				out:fade
+				class="config-container {$config.author ? 'config-container--form' : ''}"
+			>
+				{#if $config.author}
+					<Config />
+					<div class="authorbox">
+						<Author />
+					</div>
+				{:else}
 					<Author />
-				</div>
-			{:else}
-				<Author />
-			{/if}
-		</div>
+				{/if}
+			</div>
+		</OnMount>
 	</main>
 </div>
 
@@ -49,9 +75,13 @@
 
 	.intro {
 		padding: 3rem 0;
+		p {
+			font-size: 1.5rem;
+			@include bluebox;
+		}
 	}
 
-	button {
+	/*button {
 		display: inline;
 		color: var(--pink);
 		padding: 0;
@@ -61,7 +91,7 @@
 		&:hover {
 			background-color: unset;
 		}
-	}
+	}*/
 
 	.config-container {
 		max-height: 85vh;
@@ -78,7 +108,7 @@
 			display: flex;
 			flex-direction: row-reverse;
 			justify-content: end;
-			gap: .5em;
+			gap: 0.5em;
 		}
 	}
 </style>
