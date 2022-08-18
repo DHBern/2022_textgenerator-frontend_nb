8dfc6bf88750a621f0eb463297450e10c7a79483
Sebastian <sebastian@flicks.jetzt>
2022-08-18 20:12:04 +0200

    improve placeholders for qr-codes

--- src/lib/Qrcode.svelte
+++ src/lib/Qrcode.svelte
@@ -1,8 +1,19 @@
-<button>QR Codes generieren</button>
+<button>Twitter</button>
+<button>Instagram</button>
+<button>Whatsapp</button>
+<button>SMS</button>
 
 <style lang="scss">
     @use '../lib/assets/styles/mixins.scss' as *;
     button {
         @include button;
+        position: relative;
+        &:before {
+            content: 'Teilen per';
+            font-size: .8rem;
+            display: block;
+            position: absolute;
+            top: 0;
+        }
     }
 </style>
