a4e4d79d34bca506ecc6c6fdcbc0fd968e1158c3
Sebastian <sebastian@flicks.jetzt>
2022-08-18 13:02:40 +0200

    use adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		  }
 	},
 	preprocess: preprocess({
 		// ...svelte-preprocess options
